8e5b88ef845e9d68bc560a49e83c7a28be103805
FileOfName <81038683+FileOfName@users.noreply.github.com>
2026-05-11 12:18:20 +0200

    feat: minecraft 26.1

--- src/main/java/com/fileofname/hswf3/mixin/InGameHudMixin.java
+++ src/main/java/com/fileofname/hswf3/mixin/InGameHudMixin.java
@@ -1,21 +1,20 @@
 package com.fileofname.hswf3.mixin;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.InGameHud;
-
-@Mixin(InGameHud.class)
+@Mixin(Gui.class)
 public class InGameHudMixin {
 	@Shadow
-	private MinecraftClient client;
+	private Minecraft minecraft;
 
-	@Inject(at = @At("HEAD"), method = "renderScoreboardSidebar", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "displayScoreboardSidebar", cancellable = true)
 	private void hideScoreboard(final CallbackInfo info) {
-		if (client.getDebugHud().shouldShowDebugHud()) info.cancel();
+		if (minecraft.getDebugOverlay().showDebugScreen()) info.cancel();
 	}
 }
